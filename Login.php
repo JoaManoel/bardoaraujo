4a42cc2662188ab2c7a8bd666c23892f4476e38d
JoaManoel <67805223+JoaManoel@users.noreply.github.com>
2024-02-22 23:13:13 -0300

    Add files via upload

---
+++ Login.php
@@ -0,0 +1,241 @@
+<?php include_once("cabecalho.php")?>
+      <!-- Breadcrumbs -->
+      <section class="breadcrumbs-custom-inset">
+        <div class="breadcrumbs-custom context-dark bg-overlay-33">
+          <div class="container">
+            <h2 class="breadcrumbs-custom-title">Typography</h2>
+            <ul class="breadcrumbs-custom-path">
+              <li><a href="index.html">Home</a></li>
+              <li class="active">Typography</li>
+            </ul>
+          </div>
+          <div class="box-position" style="background-image: url(images/bg-breadcrumbs.jpg);"></div>
+        </div>
+      </section>
+      <!-- Base typography-->
+      <section class="section section-sm section-first bg-default text-left">
+        <div class="container">
+          <div class="row row-40 flex-lg-row-reverse justify-content-xl-between">
+            <div class="col-xl-5 d-none d-xl-block">
+              <div class="offset-left-xl-45">
+                <h1>H1 Heading</h1>
+                <h2>H2 Heading</h2>
+                <h3>H3 Heading</h3>
+                <h4>H4 Heading</h4>
+                <h5>H5 Heading</h5>
+                <h6>H6 Heading</h6>
+              </div>
+            </div>
+            <div class="col-xl-7">
+              <ul class="list-xl box-typography">
+                <li>
+                  <h1>H1 Heading</h1>
+                  <p>Welcome to our wonderful world. We sincerely hope that each and every user entering our website will find exactly what he/she is looking for. With advanced features of activating account and new login widgets, you will definitely have a great experience of using our web page.</p>
+                </li>
+                <li>
+                  <h2>H2 Heading</h2>
+                  <p>Welcome to our wonderful world. We sincerely hope that each and every user entering our website will find exactly what he/she is looking for. With advanced features of activating account and new login widgets, you will definitely have a great experience of using our web page.</p>
+                </li>
+                <li>
+                  <h3>H3 Heading</h3>
+                  <p>Welcome to our wonderful world. We sincerely hope that each and every user entering our website will find exactly what he/she is looking for. With advanced features of activating account and new login widgets, you will definitely have a great experience of using our web page.</p>
+                </li>
+                <li>
+                  <h4>H4 Heading</h4>
+                  <p>Welcome to our wonderful world. We sincerely hope that each and every user entering our website will find exactly what he/she is looking for. With advanced features of activating account and new login widgets, you will definitely have a great experience of using our web page.</p>
+                </li>
+                <li>
+                  <h5>H5 Heading</h5>
+                  <p>Welcome to our wonderful world. We sincerely hope that each and every user entering our website will find exactly what he/she is looking for. With advanced features of activating account and new login widgets, you will definitely have a great experience of using our web page.</p>
+                </li>
+                <li>
+                  <h6>H6 Heading</h6>
+                  <p>Welcome to our wonderful world. We sincerely hope that each and every user entering our website will find exactly what he/she is looking for. With advanced features of activating account and new login widgets, you will definitely have a great experience of using our web page.</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!-- HTML Text Elements-->
+      <section class="section section-sm bg-default text-left">
+        <div class="container">
+          <h3>HTML Text Elements</h3>
+          <p class="text-block">Welcome to our wonderful world. This is a bold text
+            <mark>This is a highlighted text</mark>We sincerely hope that each and every user entering our website will find exactly what he/she is looking for. With advanced features of activating account and new login<span class="tooltip-custom" data-toggle="tooltip" data-placement="top" title="Default text">Tooltips</span>widgets, you will definitely have a great experience of using our web page.<span class="text-strike">This is a strikethrough text</span><span class="text-underline">This is an underlined text.</span><a href="#">Link</a><a class="link-hover" href="#">Hover link</a><a class="link-active" href="#">Press link</a>
+          </p>
+        </div>
+      </section>
+
+      <!-- Lists and Blockquote-->
+      <section class="section section-sm section-last bg-default text-left">
+        <div class="container">
+          <div class="row row-60 row-md-80 row-lg-90">
+            <div class="col-lg-8 col-xl-6">
+              <h3>Ordered & Unordered Lists</h3>
+              <div class="row row-sm row-30">
+                <div class="col-sm-6">
+                  <ul class="list-marked">
+                    <li>Consulting</li>
+                    <li>Customer Service</li>
+                    <li>Innovation</li>
+                    <li>Management</li>
+                    <li>Ethics</li>
+                  </ul>
+                </div>
+                <div class="col-sm-6">
+                  <ol class="list-ordered">
+                    <li>Consulting</li>
+                    <li>Customer Service</li>
+                    <li>Innovation</li>
+                    <li>Management</li>
+                    <li>Ethics</li>
+                  </ol>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-8 col-xl-6">
+              <div class="offset-left-xl-65">
+                <h3>Blockquote</h3>
+                <!-- Quote Classic-->
+                <article class="quote-classic">
+                  <div class="quote-classic-text">
+                    <p class="q">We use only trusted, verified content, so you can believe our every word.</p>
+                  </div>
+                  <h6 class="quote-classic-cite">Catherine Williams</h6>
+                </article>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+<a class="section section-banner" href="https://www.templatemonster.com/intense-multipurpose-html-template.html" style="background-image: url(images/banner/background-03-1920x310.jpg); background-image: -webkit-image-set( url(images/banner/background-03-1920x310.jpg) 1x, url(images/banner/background-03-3840x620.jpg) 2x )"><img src="images/banner/foreground-03-1600x310.png" srcset="images/banner/foreground-03-1600x310.png 1x, images/banner/foreground-03-3200x620.png 2x" alt="" width="1600" height="310"></a>
+      <!-- Page Footer-->
+      <footer class="section footer-variant-2 footer-modern context-dark section-top-image section-top-image-dark">
+        <div class="footer-variant-2-content">
+          <div class="container">
+            <div class="row row-40 justify-content-between">
+              <div class="col-sm-6 col-lg-4 col-xl-3">
+                <div class="oh-desktop">
+                  <div class="wow slideInRight" data-wow-delay="0s">
+                    <div class="footer-brand"><a href="index.html"><img src="images/logo-inverse-196x42.png" alt="" width="196" height="42"/></a></div>
+                    <p>Herber is an organic farm located in California. We offer healthy foods and products to our clients.</p>
+                    <ul class="footer-contacts d-inline-block d-md-block">
+                      <li>
+                        <div class="unit unit-spacing-xs">
+                          <div class="unit-left"><span class="icon fa fa-phone"></span></div>
+                          <div class="unit-body"><a class="link-phone" href="tel:#">+1 323-913-4688</a></div>
+                        </div>
+                      </li>
+                      <li>
+                        <div class="unit unit-spacing-xs">
+                          <div class="unit-left"><span class="icon fa fa-clock-o"></span></div>
+                          <div class="unit-body">
+                            <p>Mon-Sat: 07:00AM - 05:00PM</p>
+                          </div>
+                        </div>
+                      </li>
+                      <li>
+                        <div class="unit unit-spacing-xs">
+                          <div class="unit-left"><span class="icon fa fa-location-arrow"></span></div>
+                          <div class="unit-body"><a class="link-location" href="#">4730 Crystal Springs Dr, Los Angeles, CA 90027</a></div>
+                        </div>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-6 col-lg-4 col-xl-4">
+                <div class="oh-desktop">
+                  <div class="inset-top-18 wow slideInDown" data-wow-delay="0s">
+                    <h5>Newsletter</h5>
+                    <p>Join our email newsletter for news and tips.</p>
+                    <form class="rd-form rd-mailform" data-form-output="form-output-global" data-form-type="subscribe" method="post" action="bat/rd-mailform.php">
+                      <div class="form-wrap">
+                        <input class="form-input" id="subscribe-form-5-email" type="email" name="email" data-constraints="@Email @Required">
+                        <label class="form-label" for="subscribe-form-5-email">Enter Your E-mail</label>
+                      </div>
+                      <button class="button button-block button-white" type="submit">Subscribe</button>
+                    </form>
+                    <div class="group-lg group-middle">
+                      <p class="text-white">Follow Us</p>
+                      <div>
+                        <ul class="list-inline list-inline-sm footer-social-list-2">
+                          <li><a class="icon fa fa-facebook" href="#"></a></li>
+                          <li><a class="icon fa fa-twitter" href="#"></a></li>
+                          <li><a class="icon fa fa-google-plus" href="#"></a></li>
+                          <li><a class="icon fa fa-instagram" href="#"></a></li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-3 col-xl-3">
+                <div class="oh-desktop">
+                  <div class="inset-top-18 wow slideInLeft" data-wow-delay="0s">
+                    <h5>Gallery</h5>
+                    <div class="row row-10 gutters-10" data-lightgallery="group">
+                      <div class="col-6 col-sm-3 col-lg-6">
+                        <!-- Thumbnail Classic-->
+                        <article class="thumbnail thumbnail-mary">
+                          <div class="thumbnail-mary-figure"><img src="images/gallery-image-1-129x120.jpg" alt="" width="129" height="120"/>
+                          </div>
+                          <div class="thumbnail-mary-caption"><a class="icon fl-bigmug-line-zoom60" href="images/gallery-original-7-800x1200.jpg" data-lightgallery="item"><img src="images/gallery-image-1-129x120.jpg" alt="" width="129" height="120"/></a>
+                          </div>
+                        </article>
+                      </div>
+                      <div class="col-6 col-sm-3 col-lg-6">
+                        <!-- Thumbnail Classic-->
+                        <article class="thumbnail thumbnail-mary">
+                          <div class="thumbnail-mary-figure"><img src="images/gallery-image-2-129x120.jpg" alt="" width="129" height="120"/>
+                          </div>
+                          <div class="thumbnail-mary-caption"><a class="icon fl-bigmug-line-zoom60" href="images/gallery-original-8-1200x800.jpg" data-lightgallery="item"><img src="images/gallery-image-2-129x120.jpg" alt="" width="129" height="120"/></a>
+                          </div>
+                        </article>
+                      </div>
+                      <div class="col-6 col-sm-3 col-lg-6">
+                        <!-- Thumbnail Classic-->
+                        <article class="thumbnail thumbnail-mary">
+                          <div class="thumbnail-mary-figure"><img src="images/gallery-image-3-129x120.jpg" alt="" width="129" height="120"/>
+                          </div>
+                          <div class="thumbnail-mary-caption"><a class="icon fl-bigmug-line-zoom60" href="images/gallery-original-9-800x1200.jpg" data-lightgallery="item"><img src="images/gallery-image-3-129x120.jpg" alt="" width="129" height="120"/></a>
+                          </div>
+                        </article>
+                      </div>
+                      <div class="col-6 col-sm-3 col-lg-6">
+                        <!-- Thumbnail Classic-->
+                        <article class="thumbnail thumbnail-mary">
+                          <div class="thumbnail-mary-figure"><img src="images/gallery-image-4-129x120.jpg" alt="" width="129" height="120"/>
+                          </div>
+                          <div class="thumbnail-mary-caption"><a class="icon fl-bigmug-line-zoom60" href="images/gallery-original-10-1200x800.jpg" data-lightgallery="item"><img src="images/gallery-image-4-129x120.jpg" alt="" width="129" height="120"/></a>
+                          </div>
+                        </article>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="footer-variant-2-bottom-panel">
+          <div class="container">
+            <!-- Rights-->
+            <div class="group-sm group-sm-justify">
+              <p class="rights"><span>&copy;&nbsp;</span><span class="copyright-year"></span> <span>Herber</span>. All rights reserved
+              </p>
+              <p class="rights">Design&nbsp;by&nbsp;<a href="https://www.templatemonster.com/">Templatemonster</a></p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+    <!-- Global Mailform Output-->
+    <div class="snackbars" id="form-output-global"></div>
+    <!-- Javascript-->
+    <script src="js/core.min.js"></script>
+    <script src="js/script.js"></script>
+  </body>
+</html>
